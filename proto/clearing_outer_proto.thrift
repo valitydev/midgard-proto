392a3c7f78a420e20af16e69d8637fb0290f23e6
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-06-26 16:45:08 +0300

    Update resend clearing file method signature (#13)

--- proto/clearing_outer_proto.thrift
+++ proto/clearing_outer_proto.thrift
@@ -32,6 +32,6 @@ service ClearingService {
     void StartClearingEvent(1: ClearingEvent clearingEvent) throws (1: ProviderNotFound ex1)
     /** Получение статуса клирингового события */
     ClearingEventStateResponse GetClearingEventState(1: EventID event_id) throws (1: NoClearingEvent ex1)
-    /** Повторно отправить клиринговый файл */
-    void ResendClearingFile(1: EventID event_id) throws (1: NoClearingEvent ex1)
+    /** Повторно отправить клиринговый файл для провайдера за определенный день (формат даты YYYY-MM-DD) */
+    void ResendClearingFile(1: ProviderID provider_id, 2: EventID event_id) throws (1: NoClearingEvent ex1)
 }
